0443212ba27027ed6af6a3451480c2adc1493dea
Jarno Le Conté <jarno.leconte@me.com>
2014-07-07 12:49:43 +0200

    added Functor protocol

--- Monad/Monad.playground/section-1.swift
+++ Monad/Monad.playground/section-1.swift
@@ -4,23 +4,29 @@
 
 // MONAD protocol
 
+protocol Monad {
+    typealias A
+    typealias U
+    class func ret(value: A) -> Self
+    func bind<M : Monad>(wrap: A -> M) -> M
+}
+
 operator infix >>= { associativity left }
 operator infix >> { associativity left }
 
-protocol Monad {
-    typealias A
-    typealias M
-    
-    func bind<MB : Monad>(A -> MB) -> MB
-    class func ret(value: A) -> Self
-}
-
-func >>= <MA : Monad, MB : Monad where MA.M == MB.M>(lhs: MA, rhs: MA.A -> MB) -> MB {
+func >>= <MA : Monad, MB : Monad where MA.U == MB.U>(lhs: MA, rhs: MA.A -> MB) -> MB {
     return lhs.bind(rhs)
 }
-
-func >> <MA : Monad, MB : Monad where MA.M == MB.M>(lhs: MA, rhs: MB) -> MB {
+func >> <MA : Monad, MB : Monad where MA.U == MB.U>(lhs: MA, rhs: MB) -> MB {
     return lhs.bind { _ in rhs }
+}
+
+// FUNCTOR protocol
+
+protocol Functor {
+    typealias A
+    typealias FB : Functor
+    func fmap<B>(transform: (A) -> B) -> FB
 }
 
 
@@ -38,64 +44,85 @@ enum Maybe<T> {
     }
 }
 
-extension Maybe : Monad {
+extension Maybe : Monad, Functor {
     typealias A = T
-    typealias M = Maybe<()>
-    
-    func bind<MB : Monad>(f: A -> MB) -> MB {
-        switch self {
-        case .Just(let a): return f(a);
-        case .Nothing: return Maybe.Nothing as MB;
-        }
-    }
-    
+    typealias U = Maybe<()>
+
     static func ret(value: A) -> Maybe<A> {
         return .Just(value);
     }
     
+    func bind<M : Monad>(wrap: A -> M) -> M {
+        switch self {
+        case .Just(let a): return wrap(a);
+        case .Nothing: return Maybe.Nothing as M;
+        }
+    }
     
+    func fmap<B>(transform: (A) -> B) -> Maybe<B> {
+        switch self {
+        case .Just(let a): return Maybe<B>.Just(transform(a));
+        case .Nothing: return Maybe<B>.Nothing;
+        }
+    }
 }
 
 
 // OPTIONAL conforms MONAD protocol
 
-extension Optional : Monad {
+extension Optional : Monad, Functor {
     typealias A = T
-    typealias M = Optional<()>
-    
-    func bind<MB : Monad>(f: A -> MB) -> MB {
-        switch self {
-        case .Some(let a): return f(a);
-        case .None: return Optional.None as MB;
-        }
-    }
+    typealias U = Optional<()>
     
     static func ret(value: A) -> Optional<A> {
         return .Some(value);
+    }
+    
+    func bind<M : Monad>(wrap: A -> M) -> M {
+        switch self {
+        case .Some(let a): return wrap(a);
+        case .None: return Optional.None as M;
+        }
+    }
+    
+    func fmap<B>(transform: (A) -> B) -> Optional<B> {
+        switch self {
+        case .Some(let a): return Optional<B>.Some(transform(a));
+        case .None: return Optional<B>.None;
+        }
     }
 }
 
 
 
-// examples
+
+// examples monads
 
 // Modify value within Maybe
 let example1 = Maybe.ret(5) >> Maybe.ret(6);
 let example2 = Maybe.ret(5) >>= { x in Maybe.ret(x+1) };
+example1
+example2
 
 // Change type of value within Maybe
 let example3 = Maybe.ret(5) >> Maybe.ret(true);
 let example4 = Maybe.ret(5) >>= { _ in Maybe.ret(true) };
+example3
+example4
 
 // It is NOT allowed to change the monad type (Maybe to Optional),
 // exactly what you should expect
 //let example5 = Maybe.ret(5) >> Optional.ret(5);
 //let example6 = Maybe.ret(5) >>= { _ in Optional.ret(true) };
+example5
+example6
 
 
-example1.fromJust()
-example2.fromJust()
-example3.fromJust()
-example4.fromJust()
+// examples fmap
 
-
+let example7 = Optional<Int>.Some(1).fmap({ x in x+1 });
+let example8 = Maybe<Int>.Just(7).fmap({ x in x+1 })
+let example9 = Maybe<Int>.Just(7).fmap({ x in true });
+example7
+example8
+example9
